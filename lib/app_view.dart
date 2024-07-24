5acd3112269f3b431df1456b142d4c2daeff4da7
yudhonowitanto.yw <yudhonowitanto.yw@gmail.com>
2024-07-24 09:04:55 +0000

    init commit

---
+++ lib/app_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/home_screen.dart';
+
+class MyAppView extends StatelessWidget {
+  const MyAppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'flutter demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          colorScheme: const ColorScheme.light(
+        surface: Colors.white,
+        onSurface: Colors.black,
+        primary: Colors.black45,
+        onPrimary: Colors.white,
+      )),
+      home: const HomeScreen(
+        title: 'flutter demo',
+      ),
+    );
+  }
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomeScreen> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      drawer: Drawer(
+          child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text('Drawer Header'),
+          ),
+          ListTile(
+            title: const Text('Home'),
+            selected: _selectedIndex == 0,
+            onTap: () {
+              // Update the state of the app
+              _onItemTapped(0);
+              // Then close the drawer
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: const Text('Business'),
+            selected: _selectedIndex == 1,
+            onTap: () {
+              // Update the state of the app
+              _onItemTapped(1);
+              // Then close the drawer
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: const Text('School'),
+            selected: _selectedIndex == 2,
+            onTap: () {
+              // Update the state of the app
+              _onItemTapped(2);
+              // Then close the drawer
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      )),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.account_box_rounded),
+          ),
+        ],
+        backgroundColor: Colors.red,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 10),
+              SearchAnchor(
+                  builder: (BuildContext context, SearchController controller) {
+                return SearchBarTheme(
+                  data: SearchBarThemeData(
+                    elevation: WidgetStateProperty.all(0),
+                    backgroundColor:
+                        WidgetStateProperty.all(Colors.grey.shade200),
+                    shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                            10.0), // Set the desired border radius here
+                      ),
+                    ),
+                  ),
+                  child: SearchBar(
+                    controller: controller,
+                    padding: const WidgetStatePropertyAll<EdgeInsets>(
+                        EdgeInsets.symmetric(horizontal: 16.0)),
+                    onTap: () {
+                      controller.openView();
+                    },
+                    onChanged: (_) {
+                      controller.openView();
+                    },
+                    leading: const Icon(Icons.search),
+                  ),
+                );
+              }, suggestionsBuilder:
+                      (BuildContext context, SearchController controller) {
+                return List<ListTile>.generate(5, (int index) {
+                  final String item = 'item $index';
+                  return ListTile(
+                    title: Text(item),
+                    onTap: () {
+                      setState(() {
+                        controller.closeView(item);
+                      });
+                    },
+                  );
+                });
+              }),
+              const SizedBox(height: 20),
+              Expanded(
+                child: GridView.builder(
+                  itemCount: 8,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                      childAspectRatio: 9 / 16),
+                  itemBuilder: (context, index) {
+                    return Material(
+                      color: Colors.redAccent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
